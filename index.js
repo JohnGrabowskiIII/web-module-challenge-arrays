657ccdb2ab2ca3b9b487bceaed4f7fb6394e4d5b
JohnGrabowskiIII <johngrabowski97@gmail.com>
2021-01-04 20:36:10 -0500

    finished second question and removed console log from first

--- index.js
+++ index.js
@@ -47,7 +47,6 @@ Use the copy function below to do the following:
 
 function copy(array) {
     const arrayCopy = array;
-    console.log(arrayCopy);
     return arrayCopy;
 } 
 
@@ -68,9 +67,15 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(array) {
+   if (array.length === 31) {
+       return true;
+   } else {
+       return false;
+   }
 }
+
+is31Flavors(originalFlavors);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
